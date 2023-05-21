62f844a2672a06660b009c0e92426e275236ff37
textomatic <102237745+textomatic@users.noreply.github.com>
2023-05-21 14:35:32 -0700

    Update reqs and scripts

--- classify.py
+++ classify.py
@@ -1,13 +1,21 @@
 import click
 from transformers import pipeline
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def classify_text(text):
-    classifier = pipeline(model="unitary/toxic-bert")
+    classifier = pipeline(
+        task='text-classification',
+        model='unitary/toxic-bert',
+        tokenizer='bert-base-uncased',
+        function_to_apply='sigmoid',
+        top_k=None
+    )
     result = classifier(text)
     click.echo('Classification complete!')
     click.echo('-'*50)
-    return result
+    return result[0]
 
 
 @click.command()
