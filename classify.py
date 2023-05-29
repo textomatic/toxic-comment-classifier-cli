187c4207cf562892b1991f80723c84a689e9e11c
Shen Juin Lee <102237745+textomatic@users.noreply.github.com>
2023-05-29 00:54:28 +0000

    Update CLI file and requirements.txt

--- classify.py
+++ classify.py
@@ -1,24 +1,29 @@
 import click
+import pandas as pd
 from transformers import pipeline
 import warnings
+
 warnings.filterwarnings("ignore")
 
 
+@click.command()
+@click.option("--text")
 def classify_text(text):
     classifier = pipeline(
-        task='text-classification',
-        model='unitary/toxic-bert',
-        tokenizer='bert-base-uncased',
-        function_to_apply='sigmoid',
-        top_k=None
+        task="text-classification",
+        model="unitary/toxic-bert",
+        tokenizer="bert-base-uncased",
+        function_to_apply="sigmoid",
+        top_k=None,
     )
     result = classifier(text)
-    click.echo('Classification complete!')
-    click.echo('-'*50)
-    return result[0]
+    df_result = pd.DataFrame(result[0])
+    final_df_result = df_result[df_result['score'] >= 0.5]
+    click.echo("Classification complete!")
+    click.echo("-" * 50)
+    click.echo(final_df_result)
 
 
-@click.command()
-@click.option('--text')
-def main(text):
-    click.echo(classify_text(text))
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    classify_text()
