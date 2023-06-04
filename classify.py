7355f45551c841150cc011ecf56c156b023e2b47
Shen Juin Lee <102237745+textomatic@users.noreply.github.com>
2023-06-04 22:16:32 +0000

    Added threshold option, updated tests

--- classify.py
+++ classify.py
@@ -6,7 +6,17 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def classify_text(text):
+def classify_text(text, threshold):
+    """
+    Loads the classifier model and performs classification on the given piece of text to detect toxicity within it. Classification result is returned as a Pandas DataFrame showing the toxic labels and their corresponding scores. A default threshold score of `0.5` is used for filtering out non-relevant results. If result is empty, an informative string is returned instead.
+
+    Args:
+        text(str): String for toxicity classification.
+        threshold(float): Threshold to apply for filtering classification results.
+    
+    Returns:
+        final_df_result(pd.DataFrame or str): Result of classification.
+    """
     classifier = pipeline(
         task="text-classification",
         model="unitary/toxic-bert",
@@ -16,7 +26,7 @@ def classify_text(text):
     )
     result = classifier(text)
     df_result = pd.DataFrame(result[0])
-    final_df_result = df_result[df_result["score"] >= 0.5]
+    final_df_result = df_result[df_result["score"] >= threshold]
     if len(final_df_result) == 0:
         final_df_result = "No toxicity detected!"
     return final_df_result
@@ -24,8 +34,20 @@ def classify_text(text):
 
 @click.command()
 @click.option("--text")
-def main(text):
-    result = classify_text(text)
+@click.option("--threshold", default=0.5)
+def main(text, threshold):
+    """
+    Main driver function. Accepts a string as input and performs classification on it to detect presence of toxicity.
+    Result of classification is printed to standard output through `click.echo()`.
+
+    Args:
+        text(str): Target string for toxicity classification
+        threshold(float): Threshold to apply for filtering classification results. Defaults to `0.5`, i.e. any label with a score below `0.5` is excluded.
+
+    Return:
+        None
+    """
+    result = classify_text(text, threshold)
     click.echo("Classification complete!")
     click.echo("-" * 50)
     click.echo(result)
--- test_classify.py
+++ test_classify.py
@@ -4,7 +4,7 @@ from classify import main
 
 
 @pytest.mark.parametrize(
-    "text_input, expected_labels",
+    "text_input, expected_label",
     [
         ("What an idiotic statement!", "toxic"),
         ("What an idiotic statement!", "insult"),
@@ -15,8 +15,26 @@ from classify import main
         ("He is super slim", "No toxicity detected"),
     ],
 )
-def test_main(text_input, expected_labels):
+def test_main(text_input, expected_label):
+    """Function to test the toxicity classifier CLI tool with default threshold using click CliRunner"""
+
     runner = CliRunner()
     result = runner.invoke(main, ["--text", text_input])
     assert result.exit_code == 0
-    assert expected_labels in result.output
+    assert expected_label in result.output
+
+
+@pytest.mark.parametrize(
+    "text_input, threshold_input, expected_label",
+    [
+        ("What an idiotic statement!", 0.9, "obscene"),
+        ("As fat as a giant hippo!", 0.8, "insult")
+    ],
+)
+def test_threshold(text_input, threshold_input, expected_label):
+    """Function to test the toxicity classifier CLI tool with custom threshold using click CliRunner"""
+    
+    runner = CliRunner()
+    result = runner.invoke(main, ["--text", text_input, "--threshold", threshold_input])
+    assert result.exit_code == 0
+    assert expected_label not in result.output
